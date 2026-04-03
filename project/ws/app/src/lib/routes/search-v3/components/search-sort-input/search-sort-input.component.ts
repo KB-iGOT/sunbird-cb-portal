79d8db7e3aa3fe639bf25d0077fe1c59d262b861
vishnubansaltarento <vishnu.bansal@tarento.com>
2026-04-03 11:28:51 +0530

    update for mauritius changes

--- project/ws/app/src/lib/routes/search-v3/components/search-sort-input/search-sort-input.component.ts
+++ project/ws/app/src/lib/routes/search-v3/components/search-sort-input/search-sort-input.component.ts
@@ -7,12 +7,13 @@ import {
   OnChanges,
   Output,
   ViewChild,
-} from '@angular/core';
+} from '@angular/core'
 import {
   SEARCH_SORT_DROPDOWN,
   SEARCH_SORT_PEOPLES,
-} from '../../../../../../../author/src/lib/constants/constant';
-import { SearchCategory, SearchConstantLocalStorage, SortType } from '../../models/search-v3.model';
+} from '../../../../../../../author/src/lib/constants/constant'
+import { SearchCategory, SearchConstantLocalStorage, SortType } from '../../models/search-v3.model'
+import { DomainConfService } from '@sunbird-cb/utils-v2'
 
 @Component({
   selector: 'ws-app-search-sort-input',
@@ -21,88 +22,101 @@ import { SearchCategory, SearchConstantLocalStorage, SortType } from '../../mode
 })
 export class SearchSortInputComponent implements AfterViewInit, OnChanges {
   @Output() searchSorter = new EventEmitter();
-  @Input() category!: string;
+  @Input() category!: string
   @Input() isExploreContentTab: boolean = false;
   selectedOption: string = SortType.MostRelevent;
   @Input() customOptions: any[] = [];
   options = SEARCH_SORT_DROPDOWN;
 
-  @ViewChild('sortSelect') sortSelect!: ElementRef;
+  @ViewChild('sortSelect') sortSelect!: ElementRef
 
-  constructor() {}
+  constructor(public domainConfService: DomainConfService) {
+
+  }
 
   ngOnChanges(): void {
+    const excludedValues = ['most_relevant', 'recently_added_newest']
+
+
     if (this.customOptions && this.customOptions.length > 0) {
-      this.options = this.customOptions;
-      this.selectedOption = this.customOptions[0].value;
+      this.options = this.customOptions
+      this.selectedOption = this.customOptions[0].value
     } else {
 
       if (this.category === SearchCategory.People) {
-      this.options = SEARCH_SORT_PEOPLES;
-      this.selectedOption = SortType.MostRelevent;
-      // this.searchSorter.emit(this.selectedOption);
-      } else if(this.category === SearchCategory.Communities || this.category === SearchCategory.Events) {
-        this.options = SEARCH_SORT_DROPDOWN.filter((option) => option.value !== SortType.HighestRated);
-        this.selectedOption = SortType.MostRelevent;
+        this.options = SEARCH_SORT_PEOPLES
+        this.selectedOption = SortType.MostRelevent
         // this.searchSorter.emit(this.selectedOption);
-      } else if(this.category === SearchCategory.ExternalContents) {
-        this.options = SEARCH_SORT_DROPDOWN.filter((option) => option.value !== SortType.HighestRated && 
-          option.value !== SortType.MostRelevent);
-        this.selectedOption = SortType.RecentlyAdded;
+      } else if (this.category === SearchCategory.Communities || this.category === SearchCategory.Events) {
+        this.options = SEARCH_SORT_DROPDOWN.filter((option) => option.value !== SortType.HighestRated)
+        this.selectedOption = SortType.MostRelevent
+        // this.searchSorter.emit(this.selectedOption);
+      } else if (this.category === SearchCategory.ExternalContents) {
+        this.options = SEARCH_SORT_DROPDOWN.filter((option) => option.value !== SortType.HighestRated &&
+          option.value !== SortType.MostRelevent)
+        this.selectedOption = SortType.RecentlyAdded
       }
       else {
-        this.options = SEARCH_SORT_DROPDOWN;
-        this.selectedOption = SortType.MostRelevent;
+        this.options = SEARCH_SORT_DROPDOWN
+        this.selectedOption = SortType.MostRelevent
         // this.searchSorter.emit(this.selectedOption);
         if (this.isExploreContentTab) {
-          this.options = SEARCH_SORT_DROPDOWN.filter(option => option.value !== SortType.MostRelevent);
-          this.selectedOption = SortType.RecentlyAdded;
+          this.options = SEARCH_SORT_DROPDOWN.filter(option => option.value !== SortType.MostRelevent)
+          this.selectedOption = SortType.RecentlyAdded
         } else {
-          this.options = SEARCH_SORT_DROPDOWN;
-          this.selectedOption = SortType.MostRelevent;
+          this.options = SEARCH_SORT_DROPDOWN
+          this.selectedOption = SortType.MostRelevent
         }
       }
 
-      const sortType = localStorage.getItem(SearchConstantLocalStorage.SortType);
+      const sortType = localStorage.getItem(SearchConstantLocalStorage.SortType)
       if (sortType && this.options.some((option) => option.value === sortType)) {
-        this.selectedOption = sortType;
+        this.selectedOption = sortType
         // this.searchSorter.emit(this.selectedOption);
       }
     }
 
-    const sortType = localStorage.getItem(SearchConstantLocalStorage.SortType);
+    const sortType = localStorage.getItem(SearchConstantLocalStorage.SortType)
     if (sortType && this.options.some((option) => option.value === sortType)) {
-      this.selectedOption = sortType;
+      this.selectedOption = sortType
       // this.searchSorter.emit(this.selectedOption);
     }
+
+    if (this.domainConfService?.isFeatureByPageEnabled('explore', 'showAllandContentOnly')) {
+      this.options = SEARCH_SORT_DROPDOWN.filter(
+        item => !excludedValues.includes(item.value)
+      )
+      this.selectedOption = this.options[0].value
+    }
+
   }
-  
+
   ngAfterViewInit() {
     // this.adjustSelectWidth();
   }
 
   onChange(event: Event): void {
-    this.selectedOption = (event.target as HTMLSelectElement).value;
-    this.searchSorter.emit(this.selectedOption);
+    this.selectedOption = (event.target as HTMLSelectElement).value
+    this.searchSorter.emit(this.selectedOption)
     // this.adjustSelectWidth();
   }
 
   adjustSelectWidth() {
     setTimeout(() => {
-      const select = this.sortSelect.nativeElement;
-      const selectedOption = select.options[select.selectedIndex];
-      const tempSpan = document.createElement('span');
+      const select = this.sortSelect.nativeElement
+      const selectedOption = select.options[select.selectedIndex]
+      const tempSpan = document.createElement('span')
 
-      tempSpan.style.font = window.getComputedStyle(select).font;
-      tempSpan.style.visibility = 'hidden';
-      tempSpan.style.position = 'absolute';
-      tempSpan.textContent = selectedOption.textContent;
+      tempSpan.style.font = window.getComputedStyle(select).font
+      tempSpan.style.visibility = 'hidden'
+      tempSpan.style.position = 'absolute'
+      tempSpan.textContent = selectedOption.textContent
 
-      document.body.appendChild(tempSpan);
-      const width = tempSpan.getBoundingClientRect().width;
-      document.body.removeChild(tempSpan);
+      document.body.appendChild(tempSpan)
+      const width = tempSpan.getBoundingClientRect().width
+      document.body.removeChild(tempSpan)
 
-      select.style.width = `${width + 40}px`;
-    });
+      select.style.width = `${width + 40}px`
+    })
   }
 }
